40935124a06198a28f523e0f195cba3cd77c7dcc
Giulio Starace <giulio.starace@gmail.com>
2019-11-13 12:07:05 -0300

    define plannerExternalReference resource

--- typedefs.js
+++ typedefs.js
@@ -67,7 +67,16 @@
 
 /**
  * @typedef {Object} plannerExternalReferences
- * see https://docs.microsoft.com/en-us/graph/api/resources/plannerexternalreferences?view=graph-rest-1.0
+ * @see {@link https://docs.microsoft.com/en-us/graph/api/resources/plannerexternalreferences?view=graph-rest-1.0}
+ */
+
+/**
+ * @typedef {Object} plannerExternalReference
+ * @property {String} alias A name alias to describe the reference.
+ * @property {IdentitySet} lastModifiedBy Read-only. User ID by which this is last modified.
+ * @property {String} lastModifiedDateTime Read-only. Date and time at which this is last modified. The Timestamp type represents date and time information using ISO 8601 format and is always in UTC time. For example, midnight UTC on Jan 1, 2014 would look like this: '2014-01-01T00:00:00Z'
+ * @property {String} previewPriority Used to set the relative priority order in which the reference will be shown as a preview on the task.
+ * @property {String} type Used to describe the type of the reference. Types include: PowerPoint, Word, Excel, Other.
  */
 
 /**
